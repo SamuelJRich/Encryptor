f22289fdd01e6585a729fd4e8bda92fa1df1d7b8
SamuelJRich <120282121+SamuelJRich@users.noreply.github.com>
2024-04-15 22:22:50 +0100

    feat -- created the initial root, which is the loop window for the interface to be run within, also created the frame and a test element for ensuring the setup was correct.

--- interface.py
+++ interface.py
@@ -1,3 +1,10 @@
 from tkinter import *
 from tkinter import ttk
 
+root = Tk()
+
+rtFrm = ttk.Frame(root, padding=150)
+rtFrm.grid()
+ttk.Button(rtFrm, text="Click me", command=root.destroy).grid(column=1, row=0)
+
+root.mainloop()
